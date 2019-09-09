61f156ba38640d9e7abbfd8685f1b52e6a351233
lieby weintraub <liebahinda@gmail.com>
2019-09-09 14:37:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file!
+const logDriverNames= function(drivers) {
+ return  drivers.forEach(function(element) {
+    console.log(element.name);
+  });
+};
+
+const logDriversByHometown= function (drivers, hometown) { drivers.forEach( function(element) {
+  if (element.hometown === hometown) { console.log(element.name);
+  }
+});
+}
